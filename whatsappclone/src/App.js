7cf98165f6a62fdb28e41026ebf4c4988bdc9c23
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-18 19:33:53 -0300

    parte 7

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
+import avatar1 from './imagens/avatar-1.png'
 
 export default function App() {
 
       return (
           <div className="app-janela">
               <div className="barralateral">
-                  <header>***</header>
+                  <header>
+                      <img className="cabecalho--avatar" src={avatar1} alt="avatar" />
+                      <div className="cabecalho--botoes">botoes</div>
+                  </header>
                   <div className="pesquisa">***</div>
                   <div className="listaconversas">***</div>
               </div>
