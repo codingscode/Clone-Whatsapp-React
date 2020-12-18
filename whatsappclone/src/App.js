e687632674e2bb8e5e59efd5955fab254b88731e
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-18 19:24:13 -0300

    parte 6

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -5,7 +5,11 @@ export default function App() {
 
       return (
           <div className="app-janela">
-              <div className="barralateral">****</div>
+              <div className="barralateral">
+                  <header>***</header>
+                  <div className="pesquisa">***</div>
+                  <div className="listaconversas">***</div>
+              </div>
               <div className="areaconteudo">****</div>
           </div>
       )
