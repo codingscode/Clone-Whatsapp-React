d04c313c12c4776535c6b6fe62809b204aa6881f
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-19 20:42:29 -0300

    parte 15

--- whatsappclone/src/components/itemListaConversa.js
+++ whatsappclone/src/components/itemListaConversa.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import './itemListaConversa.css'
-
+import avatar from '../imagens/avatar-1.png'
 
 
 export default function ItemListaConversa () {
 
       return (
-          <div>...</div>
+          <div className="itemListaConversa">
+               <img className="itemListaConversa--avatar" src={avatar} alt="avatar" />
+               <div className="itemListaConversa--linhas">
+                    <div className="itemListaConversa--linha" >
+                          <div className="itemListaConversa--nome">Heronnes Pereira</div>
+                          <div className="itemListaConversa--data">19:00</div>
+                    </div>
+                    <div className="itemListaConversa--linha" >
+                        <div className="itemListaConversa--ultimaMsg">
+                            <p>Opa, tudo bem ?</p>
+                        </div>
+                    </div>
+               </div>
+          </div>
       )
 }
 
