6b327e9687fc38de1ad61f1391d2d1312035d621
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-20 20:02:21 -0300

    parte 27

--- whatsappclone/src/components/IntroConversa.js
+++ whatsappclone/src/components/IntroConversa.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './IntroConversa.css'
+import imagem from '../imagens/zap.jpg'
 
 
 export default function IntroConversa() {
 
      return (
-         <div>...</div>
+         <div className="introconversa">
+             <img src={imagem} alt="imagem" />
+             <h1>Mantenha seu celular conectado</h1>
+             <h2>O Whatsapp conecta ao seu telefone para sincronizar suas mensagens. Para reduzir o uso de dados, conecte seu telefone a uma rede Wi-Fi.</h2>
+         </div>
      )
 }
 
