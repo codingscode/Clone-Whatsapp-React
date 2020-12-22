bbca69906315def9dc55ae62e4feb2712aad58a2
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-21 22:53:45 -0300

    parte 37

--- whatsappclone/src/components/JanelaConversa.js
+++ whatsappclone/src/components/JanelaConversa.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import './JanelaConversa.css'
+import SearchIcon from '@material-ui/icons/Search'
+import AttachFileIcon from '@material-ui/icons/AttachFile'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import imagem from '../imagens/avatar-1.png'
 
 
 export default function JanelaConversa() {
 
       return (
-          <div>algo....</div>
+          <div className="janelaconversa">
+               <div className="janelaconversa--cabecalho">
+                    <div className="janelaconversa--cabecalhoinfo">
+                         <img className="janelaconversa--avatar" src={imagem} alt="" />
+                         <div className="janelaconversa--nome">Heronnes Pereira</div>
+                    </div>
+                    <div className="janelaconversa--cabecbotoes">
+                         <div className="janelaconversa--botao">
+                             <SearchIcon style={{color: '#919191'}} />
+                         </div>
+                         <div className="janelaconversa--botao">
+                             <AttachFileIcon style={{color: '#919191'}} />
+                         </div>
+                         <div className="janelaconversa--botao">
+                             <MoreVertIcon style={{color: '#919191'}} />
+                         </div>
+                    </div>
+               </div>
+               <div className="janelaconversa--corpo">
+
+               </div>
+               <div className="janelaconversa--rodape">
+
+               </div>
+          </div>
       )
 }
 
