5dd31bf3470496f01ece79ec97624b9432221e25
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-22 21:02:26 -0300

    parte 42

--- whatsappclone/src/components/JanelaConversa.js
+++ whatsappclone/src/components/JanelaConversa.js
@@ -4,6 +4,11 @@ import SearchIcon from '@material-ui/icons/Search'
 import AttachFileIcon from '@material-ui/icons/AttachFile'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import imagem from '../imagens/avatar-1.png'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import CloseIcon from '@material-ui/icons/Close'
+import SendIcon from '@material-ui/icons/Send'
+import MicIcon from '@material-ui/icons/Mic'
+
 
 
 export default function JanelaConversa() {
@@ -31,7 +36,19 @@ export default function JanelaConversa() {
 
                </div>
                <div className="janelaconversa--rodape">
+                    <div className="janelaconversa--pre">
+                        <div className="janelaconversa--botao">
+                            <InsertEmoticonIcon style={{color: '#919191'}} />
+                        </div>
+                    </div>
+                    <div className="janelaconversa--areaentrada">
 
+                    </div>
+                    <div className="janelaconversa--pos">
+                        <div className="janelaconversa--botao">
+                            <SendIcon style={{color: '#919191'}} />
+                        </div>
+                    </div>
                </div>
           </div>
       )
