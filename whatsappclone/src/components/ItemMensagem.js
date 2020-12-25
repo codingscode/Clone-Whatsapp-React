dda9501631baf43100fbc4ba019adda2ceb29c48
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-24 21:46:29 -0300

    parte 67

--- whatsappclone/src/components/ItemMensagem.js
+++ whatsappclone/src/components/ItemMensagem.js
@@ -6,7 +6,7 @@ export default function ItemMensagem () {
      return (
          <div className="linhamensagem">
               <div className="itemMensagem">
-                  <div className="textomensagem">Bla bla bla</div>
+                  <div className="textomensagem">Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla</div>
                   <div className="datamensagem">19:00</div>
               </div>
          </div>
