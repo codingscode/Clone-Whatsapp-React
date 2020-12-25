d85a7eaedcca0403b78e8108077a580ca969be96
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-25 13:53:13 -0300

    parte 76

--- whatsappclone/src/components/NovaConversa.js
+++ whatsappclone/src/components/NovaConversa.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './NovaConversa.css'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+
 
 export default function NovaConversa( ) {
 
       return (
           <div className="novaconversa">
               <div className="novaconversa--cabecalho">
-                  <div className="novaconversa--botaovoltar"></div>
+                  <div className="novaconversa--botaovoltar">
+                       <ArrowBackIcon style={{color: '#FFFFFF'}} />
+                  </div>
                   <div className="novaconversa--titulocabecalho">Nova Conversa</div>
               </div>
               <div className="novaconversa--lista"></div>
