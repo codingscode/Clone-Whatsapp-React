accabd15923dee3a4f1f7275ca2c68608d73eedd
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-25 14:25:12 -0300

    parte 80

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -8,26 +8,28 @@ import SearchIcon from '@material-ui/icons/Search'
 import ItemListaConversa from './components/itemListaConversa'
 import IntroConversa from './components/IntroConversa.js'
 import JanelaConversa from './components/JanelaConversa'
-import avatar from './imagens/avatar-3.png'
-import perfil from './imagens/avatar-4.png'
+import avatar1 from './imagens/avatar-1.png'
+import avatar2 from './imagens/avatar-2.png'
+import avatar3 from './imagens/avatar-3.png'
+import avatar4 from './imagens/avatar-4.png'
 import NovaConversa from './components/NovaConversa'
 
 
 export default function App() {
 
       const [listaconversas, setListaconversas] = useState([
-          {chatId: 1, titulo: 'Fulanis', imagem: avatar},
-          {chatId: 2, titulo: 'Ciclanis', imagem: avatar},
-          {chatId: 3, titulo: 'Beltranis', imagem: avatar},
-          {chatId: 4, titulo: 'Carinha', imagem: avatar}
+          {chatId: 1, titulo: 'Fulanis', imagem: avatar3},
+          {chatId: 2, titulo: 'Ciclanis', imagem: avatar3},
+          {chatId: 3, titulo: 'Beltranis', imagem: avatar3},
+          {chatId: 4, titulo: 'Carinha', imagem: avatar3}
       ])
       const [conversaAtiva, setConversaAtiva] = useState({})
-      const [usuario, setUsuario] = useState({ id: 1234, avatar: perfil, nome: 'Heronnes Pereira' })
+      const [usuario, setUsuario] = useState({ id: 1234, avatar: avatar4, nome: 'Heronnes Pereira' })
 
       return (
           <div className="app-janela">
               <div className="barralateral">
-                  <NovaConversa />
+                  <NovaConversa avatares={[avatar1, avatar2, avatar3, avatar4]}/>
                   <header>
                       <img className="cabecalho--avatar" src={usuario.avatar} alt="avatar" />
                       <div className="cabecalho--botoes">
--- whatsappclone/src/components/NovaConversa.js
+++ whatsappclone/src/components/NovaConversa.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NovaConversa.css'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+/* import avatares from '../imagens/' */
 
+export default function NovaConversa({ avatares }) {
 
-export default function NovaConversa( ) {
+      const [lista, setLista] = useState([
+          {id: 123, avatar: avatares[0], nome: 'Heronnes Pereira'},
+          {id: 123, avatar: avatares[1], nome: 'Fulanis'},
+          {id: 123, avatar: avatares[2], nome: 'Beltranis'},
+          {id: 123, avatar: avatares[3], nome: 'Ciclanis'}
+      ])
 
       return (
           <div className="novaconversa">
@@ -13,7 +20,14 @@ export default function NovaConversa( ) {
                   </div>
                   <div className="novaconversa--titulocabecalho">Nova Conversa</div>
               </div>
-              <div className="novaconversa--lista"></div>
+              <div className="novaconversa--lista">
+              {lista.map((cada, chave) => (
+                    <div className="novaconversa--item" key={chave}>
+                         <img className="novaconversa--itemavatar" src={cada.avatar} alt="avatar" />
+                         <div className="novaconversa--itemnome">{cada.nome}</div>
+                    </div>
+               ))}
+              </div>
           </div>
       )
 }
