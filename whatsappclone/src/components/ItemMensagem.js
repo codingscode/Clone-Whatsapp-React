ce98851fc631075d8a3541599ea733f854634402
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-25 12:31:02 -0300

    parte 72

--- whatsappclone/src/components/ItemMensagem.js
+++ whatsappclone/src/components/ItemMensagem.js
@@ -5,7 +5,7 @@ export default function ItemMensagem ({ dados, usuario }) {
 
      return (
          <div className="linhamensagem" style={{justifyContent: usuario.id === dados.autor ? 'flex-end' : 'flex-start'}} >
-              <div className="itemMensagem">
+              <div className="itemMensagem" style={{backgroundColor: usuario.id === dados.autor ? '#DCF8C6' : 'FFF'}} >
                   <div className="textomensagem">{dados.corpo}</div>
                   <div className="datamensagem">19:00</div>
               </div>
