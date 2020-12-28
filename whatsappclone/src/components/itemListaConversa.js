c17aeb9feed13e72da9f71ecb50ee5e745ab99ae
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-28 11:30:24 -0300

    parte 93

--- whatsappclone/src/components/itemListaConversa.js
+++ whatsappclone/src/components/itemListaConversa.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './itemListaConversa.css'
 import avatar from '../imagens/avatar-1.png'
 
 
 export default function ItemListaConversa ({ clicar, ativo, dados }) {
+      const [tempo, setTempo] = useState('')
+
+      useEffect(() => {
+            if (dados.dataUltimaMensagem > 0) {
+                let d = new Date(dados.dataUltimaMensagem.seconds*1000)
+                let horas = d.getHours()
+                let minutos = d.getMinutes()
+                horas = horas < 10 ? `0${horas}` : horas
+                minutos = minutos < 10 ? `0${minutos}` : minutos
+                setTempo(`${horas}:${minutos}`)
+            }
+      }, [dados])
 
       return (
           <div className={`itemListaConversa ${ativo ? 'ativo' : ''}`} onClick={clicar}>
@@ -11,7 +23,7 @@ export default function ItemListaConversa ({ clicar, ativo, dados }) {
                <div className="itemListaConversa--linhas">
                     <div className="itemListaConversa--linha" >
                           <div className="itemListaConversa--nome">{dados.titulo}</div>
-                          <div className="itemListaConversa--data">19:00</div>
+                          <div className="itemListaConversa--data">{tempo}</div>
                     </div>
                     <div className="itemListaConversa--linha" >
                         <div className="itemListaConversa--ultimaMsg">
